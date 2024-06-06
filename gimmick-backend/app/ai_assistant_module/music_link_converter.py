320845b472e0656328592e3e8628f6eb6cb1e588
dianamocanu05 <diana.paula.mocanu@gmail.com>
2024-06-06 09:59:36 +0300

    music converter module done

--- gimmick-backend/app/ai_assistant_module/music_link_converter.py
+++ gimmick-backend/app/ai_assistant_module/music_link_converter.py
@@ -1,36 +1,12 @@
 import spotipy
+from googleapiclient.discovery import build
 from spotipy import SpotifyClientCredentials
+from yt_dlp import YoutubeDL
 
-from app.ai_assistant_module.settings import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
-
-class SpotifyService: pass
-class YoutubeService: pass
-class Song:
-
-    def __init__(self, title, artist, album):
-        self.title = title
-        self.artist = artist
-        self.album = album
-        self.youtube_link = None
-        self.spotify_link = None
-        self.added_by_user = None
-        self.added_timestamp = None
+from app.ai_assistant_module.settings import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, YOUTUBE_API_KEY
 
 
-
-class MusicLinkConverter:
-
-    def __init__(self):
-        self.song = None
-
-    def find_song(self, link):
-        if 'spotify' in link:
-            self.song = SpotifyToYoutubeConverter().get_song_by_link(link)
-        elif 'youtube' in link:
-            #self.song = YoutubeToSpotifyConverter().get_song_by_link(link)
-            pass
-
-class SpotifyToYoutubeConverter():
+class SpotifyService:
     def __init__(self):
         self.auth_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
         self.sp = spotipy.Spotify(auth_manager=self.auth_manager)
@@ -42,15 +18,83 @@ class SpotifyToYoutubeConverter():
         album_name = track['album']['name']
         song_name = track['name']
 
-        song = Song(song_name, artist_name, album_name)
+        song = Song(song_name, artist_name)
+        song.album = album_name
         song.spotify_link = spotify_link
         return song
 
+    def get_link_by_song(self, song):
+        searchQuery = song.title + ' ' + song.artist + ' ' + song.album
+        searchResults = self.sp.search(q=searchQuery, type="track")
+        return searchResults['tracks']['items'][0]['external_urls']['spotify']
 
+
+class YoutubeService:
+    def __init__(self):
+        self.youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+    def get_song_by_link(self, youtube_link):
+        with YoutubeDL() as ydl:
+            info_dict = ydl.extract_info(youtube_link, download=False)
+            title = info_dict.get('title', None)
+            artist = info_dict.get('artist', None)
+            album = info_dict.get('album', None)
+            song = Song(title, artist)
+            if album is not None:
+                song.album = album
+            return song
+
+    def get_link_by_song(self, song):
+        query = f"{song.title} {song.artist}"
+        request = self.youtube.search().list(
+            part='snippet',
+            q=query,
+            type='video',
+            maxResults=1
+        )
+        response = request.execute()
+        if response['items']:
+            video_id = response['items'][0]['id']['videoId']
+            video_url = f"https://www.youtube.com/watch?v={video_id}"
+            return video_url
+        return None
+
+
+class Song:
+
+    def __init__(self, title, artist):
+        self.title = title
+        if artist is not None:
+            self.artist = artist
+        else:
+            self.artist = ""
+        self.album = ""
+        self.youtube_link = None
+        self.spotify_link = None
+        self.added_by_user = None
+        self.added_timestamp = None
+
+    def to_string(self):
+        print(self.title + " - " + self.artist + " (" + self.album + ")")
+
+
+def convert(link):
+    if 'spotify' in link:
+        song = SpotifyService().get_song_by_link(link)
+        song.spotify_link = link
+        youtube_link = YoutubeService().get_link_by_song(song)
+        song.youtube_link = youtube_link
+        return youtube_link, song
+    elif 'youtube' in link:
+        song = YoutubeService().get_song_by_link(link)
+        song.youtube_link = link
+        spotify_link = SpotifyService().get_link_by_song(song)
+        song.spotify_link = spotify_link
+        return spotify_link, song
+    else:
+        return None, None
 
 
 if __name__ == "__main__":
-    msl = MusicLinkConverter()
-    msl.convert('https://open.spotify.com/track/4ipzXe2jmoeN7zm2VMb4eX?si=834133550b8b4b7c')
-
-#https://open.spotify.com/album/5OZ44LaqZbpP3m9B3oT8br?si=sa66CyVGRW2QNHLYWb0oXg
+    converted_link, _ = convert('https://open.spotify.com/track/0hWCzWl04zT7P6vMy63XCN?si=344687514d914991')
+    print(converted_link)
